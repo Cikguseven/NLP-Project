32fc23ce14150806722a230a699c2975fac6cbad
Kieron Seven Lee <40373653+Cikguseven@users.noreply.github.com>
2022-04-20 14:42:00 +0800

    z

--- OLID/reddit_v0/offensive_pos_tags_analysis.py
+++ OLID/reddit_v0/offensive_pos_tags_analysis.py
@@ -3,89 +3,107 @@ import spacy
 import comment_filter
 from os import listdir
 from collections import Counter
-import tag_freq_analysis
 
-accumulator = Counter()
+accumulator_non = Counter()
+accumulator_off = Counter()
 
-# custom_models = [f for f in listdir(
-#     main_config.NER_model_directory) if 'v' in f]
-
-
-olid_training_data = main_config.training_tweets_getter()
+tags_dict = {"$": 10,
+             "''": 11,
+             ",": 12,
+             "-LRB-": 13,
+             "-RRB-": 14,
+             ".": 15,
+             ":": 16,
+             "ADD": 17,
+             "AFX": 18,
+             "CC": 19,
+             "CD": 20,
+             "DT": 21,
+             "EX": 22,
+             "FW": 23,
+             "HYPH": 24,
+             "IN": 25,
+             "JJ": 26,
+             "JJR": 27,
+             "JJS": 28,
+             "LS": 29,
+             "MD": 30,
+             "NFP": 31,
+             "NN": 32,
+             "NNP": 33,
+             "NNPS": 34,
+             "NNS": 35,
+             "PDT": 36,
+             "POS": 37,
+             "PRP": 38,
+             "PRP$": 39,
+             "RB": 40,
+             "RBR": 41,
+             "RBS": 42,
+             "RP": 43,
+             "SYM": 44,
+             "TO": 45,
+             "UH": 46,
+             "VB": 47,
+             "VBD": 48,
+             "VBG": 49,
+             "VBN": 50,
+             "VBP": 51,
+             "VBZ": 52,
+             "WDT": 53,
+             "WP": 54,
+             "WP$": 55,
+             "WRB": 56,
+             "XX": 57,
+             "``": 58}
 
 non_offensive_tweets = []
+non_values = []
+offensive_tweets = []
+off_values = []
+
+data = [(non_offensive_tweets, 0, non_values), (offensive_tweets, 1, off_values)]
+
+olid_training_data = main_config.training_tweets_getter()
 
 for tweet in olid_training_data:
     if tweet[2] != 'OFF':
         non_offensive_tweets.append(tweet[1])
+    else:
+        offensive_tweets.append(tweet[1])
 
-print(len(non_offensive_tweets))
-
-filtered_tweets = main_config.preprocess(non_offensive_tweets, False)
-
-tags_dict = {"$": 1,
-             "''": 2,
-             ",": 3,
-             "-LRB-": 4,
-             "-RRB-": 5,
-             ".": 6,
-             ":": 7,
-             "ADD": 8,
-             "AFX": 9,
-             "CC": 10,
-             "CD": 11,
-             "DT": 12,
-             "EX": 13,
-             "FW": 14,
-             "HYPH": 15,
-             "IN": 16,
-             "JJ": 17,
-             "JJR": 18,
-             "JJS": 19,
-             "LS": 20,
-             "MD": 21,
-             "NFP": 22,
-             "NN": 23,
-             "NNP": 24,
-             "NNPS": 25,
-             "NNS": 26,
-             "PDT": 27,
-             "POS": 28,
-             "PRP": 29,
-             "PRP$": 30,
-             "RB": 31,
-             "RBR": 32,
-             "RBS": 33,
-             "RP": 34,
-             "SYM": 35,
-             "TO": 36,
-             "UH": 37,
-             "VB": 38,
-             "VBD": 39,
-             "VBG": 40,
-             "VBN": 41,
-             "VBP": 42,
-             "VBZ": 43,
-             "WDT": 44,
-             "WP": 45,
-             "WP$": 46,
-             "WRB": 47,
-             "XX": 48,
-             "``": 49}
+non_offensive_tweets = main_config.preprocess(non_offensive_tweets, False)
+offensive_tweets = main_config.preprocess(offensive_tweets, False)
 
 spacy.require_gpu()
 nlp = spacy.load('en_core_web_trf')
-docs = list(nlp.pipe(filtered_tweets))
-all_tags = [[tags_dict[tok.tag_] for tok in doc] for doc in docs]
 
-print(all_tags)
+for sets in data:
+    docs = list(nlp.pipe(sets[0]))
+    all_tags = [[tags_dict[tok.tag_] for tok in doc] for doc in docs]
+    for pattern in all_tags:
+        length = len(pattern)
+        if length > 5:
+            for l in range(4, length + 1):
+                for start in range(length - l):
+                    if sets[1] == 0:
+                        accumulator_non[tuple(pattern[start:start+l])] += 1
+                    else:
+                        accumulator_off[tuple(pattern[start:start+l])] += 1
 
-# for pattern in test1.results:
-#     length = len(pattern)
-#     if length > 5:
-#         for l in range(8, length + 1, 3):
-#             for start in range(length - l):
-#                 accumulator[pattern[start:start+l]] += 1
+non_values = [[get_key(y) for y in x[0]] for x in accumulator_non.most_common(80)]
+off_values = [[get_key(y) for y in x[0]] for x in accumulator_off.most_common(80)]
 
-# print(accumulator.most_common(40))
+print(non_values)
+print(off_values)
+
+
+def get_key(val):
+    for key, value in tags_dict.items():
+         if val == value:
+             return key
+
+    return "key doesn't exist"
+
+
 
