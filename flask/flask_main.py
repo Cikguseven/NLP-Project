15af99184ef5555c585d4164e91f03b3d7a77d2d
Kieron Seven Lee <40373653+Cikguseven@users.noreply.github.com>
2022-05-19 12:22:38 +0800

    a

--- flask/flask_main.py
+++ flask/flask_main.py
@@ -71,5 +71,18 @@ def predict():
     # return render_template('home.html', display_right = True, input = input_text, render_result = result_array)
 
 
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+    
+    
+@app.get('/shutdown')
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
+
+
 if __name__ == "__main__":
     app.run(debug=True)
